9272cbdca2e38228572a8940642bd3773c7839e0
frian <a@frian.org>
2017-03-30 15:19:21 +0200

    ordered use statements

--- Command/RecreateDoctrineDatabaseCommand.php
+++ Command/RecreateDoctrineDatabaseCommand.php
@@ -4,10 +4,10 @@
 namespace Frian\ConsoleUtilsBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class RecreateDoctrineDatabaseCommand extends Command
 {
